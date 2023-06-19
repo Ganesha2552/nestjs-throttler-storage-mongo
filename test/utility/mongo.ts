8726e1f0d4bed682331bf80d85c9d0dfe3fb6bca
Ganesha moorthy <ganesh.moorthy2552@gmail.com>
2023-06-20 01:02:48 +0530

    Update mongo.ts

--- test/utility/mongo.ts
+++ test/utility/mongo.ts
@@ -1,5 +1,5 @@
 export const mongoURL =
-  'mongodb://softblobs:softblobs@ac-id5uem2-shard-00-00.rvttqf3.mongodb.net:27017,ac-id5uem2-shard-00-01.rvttqf3.mongodb.net:27017,ac-id5uem2-shard-00-02.rvttqf3.mongodb.net:27017/?ssl=true&replicaSet=atlas-yad96h-shard-0&authSource=admin&retryWrites=true&w=majority';
+  'mongodb://localhost';
 export const mongoDB = 'test';
 export const mongoCollectionName = 'throttler';
 export const mongoExpireAtIndex = 'expireAt';
